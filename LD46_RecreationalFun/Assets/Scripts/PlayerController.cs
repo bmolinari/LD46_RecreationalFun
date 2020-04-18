988eccffafb3b275a7f2182e29ab483b611557aa
Brendan M <b.molinari@outlook.com>
2020-04-17 22:48:54 -0400

    Added Player sprite/Prefab. Simple player movement script.

---
+++ LD46_RecreationalFun/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed;
+
+    private Vector2 moveDirection;
+    private Rigidbody2D rb;
+
+    public void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInputs();
+        Move();
+    }
+
+    private void Move()
+    {
+        rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+    }
+
+    private void ProcessInputs()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        moveDirection = new Vector2(moveX, moveY).normalized;
+    }
+}
