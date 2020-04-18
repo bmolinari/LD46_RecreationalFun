c7ee115a0acd9af3c2e5d1dfcb4028a5629b1a55
Brendan M <b.molinari@outlook.com>
2020-04-17 23:18:14 -0400

    Character now rotates to where the mouse is aiming.

--- LD46_RecreationalFun/Assets/Scripts/PlayerController.cs
+++ LD46_RecreationalFun/Assets/Scripts/PlayerController.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    public Camera sceneCamera;
+    private Rigidbody2D rb;
+
     [SerializeField]
     private float moveSpeed;
 
     private Vector2 moveDirection;
-    private Rigidbody2D rb;
+    private Vector2 mousePosition;
 
     public void Awake()
     {
@@ -19,12 +23,21 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         ProcessInputs();
+    }
+
+
+    private void FixedUpdate()
+    {
         Move();
     }
 
     private void Move()
     {
         rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+
+        Vector2 aimDirection = mousePosition - rb.position;
+        float aimAngle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = aimAngle;
     }
 
     private void ProcessInputs()
@@ -33,5 +46,6 @@ public class PlayerController : MonoBehaviour
         float moveY = Input.GetAxisRaw("Vertical");
 
         moveDirection = new Vector2(moveX, moveY).normalized;
+        mousePosition = sceneCamera.ScreenToWorldPoint(Input.mousePosition);
     }
 }
