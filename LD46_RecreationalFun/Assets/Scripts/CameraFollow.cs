8d05ccb9c170090b7838084c6ab4b1602bd36840
Brendan M <b.molinari@outlook.com>
2020-04-17 22:59:46 -0400

    Added smooth camera follow.

---
+++ LD46_RecreationalFun/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+
+    public float smoothSpeed = 0.075f;
+    public Vector3 offset;
+
+    private void FixedUpdate()
+    {
+        Vector3 targetPosition = target.position + offset;
+        Vector3 offsetPosition = Vector3.Lerp(transform.position, targetPosition, smoothSpeed);
+        transform.position = offsetPosition;
+    }
+}
