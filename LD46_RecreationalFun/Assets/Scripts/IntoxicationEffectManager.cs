4879b9307511bbb643f43f78f6b66633038fcbf2
Brendan M <b.molinari@outlook.com>
2020-04-18 23:02:14 -0400

    Added Universal Render Pipeline for post processing effects and 2D lights.

---
+++ LD46_RecreationalFun/Assets/Scripts/IntoxicationEffectManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class IntoxicationEffectManager : MonoBehaviour
+{
+    public PlayerToxicity playerToxicity;
+
+    public PostProcessVolume globalPostProcessing;
+
+
+    private ChromaticAberration chromaticAbberation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        globalPostProcessing.profile.TryGetSettings(out chromaticAbberation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.J))
+        {
+            chromaticAbberation.intensity.value = .5f;
+        }
+    }
+}
